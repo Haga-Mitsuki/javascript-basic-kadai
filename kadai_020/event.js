4f71fcb88ab0622bb0a35d31bb33f55b05f61097
芳賀光樹 <129939277+Haga-Mitsuki@users.noreply.github.com>
2025-05-19 19:11:32 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,6 @@
-const btn = document.getElementById('output-btn');
+const btn = document.getElementById('btn');
 
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
-  console.log('ボタンをクリックしました');
+  text.textContent = "ボタンをクリックしました"
 });
