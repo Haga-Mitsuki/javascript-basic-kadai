fe112667e0f0bddc9651334ee36a3e8952b8806e
芳賀光樹 <129939277+Haga-Mitsuki@users.noreply.github.com>
2025-09-19 19:32:37 +0900

    Update if-switch.js

--- javascript-basic/js_6/js/if-switch.js
+++ javascript-basic/js_6/js/if-switch.js
@@ -1,5 +1,13 @@
-// 算術演算子を使った場合の戻り値を出力する
-console.log(45 + 18);
+// 体重（kg）
+let weight = 68;
 
-// 比較演算子を使った場合の戻り値を出力する
-console.log(45 > 18);
+// 身長（m）
+let height = 1.7;
+
+// BMIの計算式：体重 ÷ (身長 × 身長)
+let bmi = weight / (height * height);
+
+// 結果をコンソールに出力
+console.log("体重: " + weight + "kg");
+console.log("身長: " + height + "m");
+console.log("BMI: " + bmi);
